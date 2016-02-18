d71abf73f866f4359ab3a6288ea87ded1919c4fe
squeetus <burlinsond2@gmail.com>
2016-02-17 19:46:20 -0800

    filtering for industry sector works properly, filtering by chemical filters correct facility but still aggregates all usage for each kept facility.

--- src/scripts/modules/filter.js
+++ src/scripts/modules/filter.js
@@ -20,7 +20,7 @@ define(function() {
   /* filter values */
   var filters = {
     industry: [2211],
-    chemical: [],
+    chemical: ["71432", "N100"],
     usage: []
   };
 
@@ -57,27 +57,80 @@ define(function() {
         chartData[i] = 0;
       }
 
-      for(var f in data) {
-        for(var j = 0; j < data[f].stack.length; j++) {
-          chartData[j] += data[f].stack[j] + data[f].fugitive[j];
+      for( var f in data ) {
+        for( var j = 0; j < data[ f ].stack.length; j++ ) {
+          chartData[j] += data[ f ].stack[ j ] + data[ f ].fugitive[ j ];
         }
       }
 
       return chartData;
   }
 
-  function filter(data) {
-    var fData = data.facilities;
-
-
-
-    /* filter by particular filter settings */
-    // for(var f in config) {
-    //   switch(f) {
-    //     case "regulation":
-    //       fData = filterByRegulation(config[f]);
-    //   }
+  function filter( data ) {
+    var fData = [],
+      i = 0, // loop counter
+      j = 0, // loop counter
+      facility = null,
+      industryMatch = false,
+      chemicalMatch = false;
+    //
+    // for( i = 0; i < data.facilities.length; i++ ) {
+    //   fData[i] = data.facilities[i];
     // }
+    console.log( "STARTING with ", data.facilities.length, "facilities" );
+
+    for( var fac in data.facilities ) {
+      facility = data.facilities[ fac ];
+      industryMatch = false;
+      chemicalMatch = false;
+      // filter industry sector
+
+      if( filters.industry.length > 0 ) { // only filter by industry if industry filters exist
+        for( i = 0; i < filters.industry.length; i++ ) {      // for each filter industry
+          if( facility.industry.indexOf(filters.industry[ i ]) === 0 ) {  // keep if industry matches facility
+            industryMatch = true;
+            // console.log(industryMatch);
+            break;    // stop looking at industries
+          }
+        }
+        if( !industryMatch ) { // facility does not match filtered industry sectors
+          // console.log( facility, "does not conform to industry filter" );
+          // // fData.splice( fac, 1 );
+          // console.log( fac );
+          // toDelete++;
+          continue;
+        }
+      } else
+        industryMatch = true;
+
+      // filter chemicals
+      if( filters.chemical.length > 0 ) { // only filter by chemical if chemical filters exist
+        for( i = 0; i < filters.chemical.length; i++ ) {    // for each filter chemical
+          for( j = 0; j < facility.chemicals.length; j++ ) { // see if the chemical is in the facility
+            if( filters.chemical[ i ] == facility.chemicals[ j ].chemical ) { // keep if a chemical matches
+              // console.log("keeping", facility);
+              chemicalMatch = true;
+              break;
+            }
+          }
+        }
+        if( !chemicalMatch ) { // facility does not match filtered industry sectors
+          // console.log( fac, "does not conform to chemical filter.. deleting" );
+          // fData.splice( fac, 1 );
+          // toDelete++;
+          continue;
+        }
+      } else
+        chemicalMatch = true;
+
+      if(chemicalMatch && industryMatch) {
+        fData.push( facility );
+      }
+    } // finished filtering facilities
+
+    console.log( "Filtered down to ", fData.length, "facilities:", fData);
+
+    // filter aggregation by chemical still
     return aggregateUsage(fData);
   }
 
